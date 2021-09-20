69c5f2051b60ec7632888b9f37817637187635d2
Brittany Fenney <brittanyfenney@gmail.com>
2021-09-20 11:09:31 -0700

    display one random photo from NASA API

--- client/App.js
+++ client/App.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { apiKey } from '../secrets'
 
 export const App = () => {
+  const [photos, setPhotos] = useState([])
+
+  const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=1`
+
+  async function getPhotos() {
+    try {
+      const response = await fetch(apiUrl)
+      const data = await response.json()
+      setPhotos(data)
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
+  useEffect(() => {
+    getPhotos()
+  }, [])
 
   return (
     <div>
       <h1>Spacestagram</h1>
+      <div className="image-container">
+        {photos && photos.length ? photos.map(photo => {
+          return (
+            <a href={photo.url} key={photo.title}>
+            <img src={photo.url} />
+            </a>
+
+          )
+
+        })
+      : "Looking for photos"}
+      </div>
     </div>
   )
 }
